08fe4f1608fc77908a1abd0583990223768ef638
shahid <shahdbest@gmail.com>
2022-08-19 16:07:31 +0530

    a

---
+++ src/main/java/com/stackroute/lamdbaexpression/PredicateFunctionalInterface.java
@@ -0,0 +1,10 @@
+package com.stackroute.lamdbaexpression;
+
+import java.util.List;
+
+public class PredicateFunctionalInterface {
+    //write logic to find the values that starts with letter I in the given list
+    public List<String> findPattern(List<String> list) {
+        return null;
+    }
+}
